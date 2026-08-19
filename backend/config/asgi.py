266c6b424ca29c577d9e5fc7e79c374e3d1e3cff
SANDYY2002 <Sandeshchhetri999@gmail.com>
2026-08-19 23:46:31 +0545

    fix: use JWT authentication for websocket routing

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,6 +1,5 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
@@ -9,10 +8,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django_application = get_asgi_application()
 
 from apps.conversations.routing import websocket_urlpatterns
+from config.websocket_auth import JWTAuthMiddleware
 
 application = ProtocolTypeRouter(
     {
         "http": django_application,
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        "websocket": JWTAuthMiddleware(URLRouter(websocket_urlpatterns)),
     }
 )
